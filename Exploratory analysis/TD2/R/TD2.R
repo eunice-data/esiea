3195e2e5e0c78e6f6b7593d419174f0a026fcd94
Valentin Mouret <valentin@monbanquet.fr>
2017-09-11 14:12:46 +0200

    renamed directory

---
+++ Exploratory analysis/TD2/R/TD2.R
@@ -0,0 +1,7 @@
+dir_path <- "esiea/Exploratory analysis/TD2/"
+csv_1_path <- paste(dir_path, "afc_tache_menageres.csv", sep="")
+
+dataP <- read.csv(csv_1_path, sep=";")
+dataP
+
+barplot(t(dataP[, -1]), beside =T, names = dataP$Task, col = c("lightblue", "mistyrose","lightcyan", "lavender"), legend = colnames(dataP [,2:5]))
