917afde37b8be1b977a30c60d0121aa451cd8a7f
Valentin Mouret <valentin@monbanquet.fr>
2017-09-11 15:48:24 +0200

    Readme to the summary

--- Exploratory analysis/TD2/R/README.md
+++ Exploratory analysis/TD2/R/README.md
@@ -2,6 +2,23 @@
 
 ## Tâches ménagères
 
+### Chargement des données
+
+On importe les données dans un dataframe:\
+`df <- read.csv(csv_1_path, sep=";", row.names=1)`
+
+On résumme les données:\
+`summary(df)`
+
+| | Wife | Alternating | Husband | Jointly
+| --- | --- | --- | --- | --- |
+| Min. | 0.00 | 1.00 | 1.00 | 2.00
+|1st Qu. | 10.00 | 11.00 | 5.00 | 4.00
+|Median  | 32.00 | 14.00 | 9.00 | 15.00
+|Mean    | 46.15 | 19.54 | 29.31 | 39.15
+|3rd Qu. | 77.00 | 24.00 | 23.00 | 57.00
+|Max.    | 156.00 | 51.00 | 160.00 | 153.00
+
 Histogramme obtenu:
 ![figure1.png](https://github.com/ValentinMouret/esiea/blob/master/Exploratory%20analysis/TD2/R/pictures/bar_plot_1.png)
 
--- Exploratory analysis/TD2/R/TD2.R
+++ Exploratory analysis/TD2/R/TD2.R
@@ -1,7 +1,28 @@
-dir_path <- "esiea/Exploratory analysis/TD2/"
+dir_path <- "esiea/Exploratory analysis/TD2/data/"
 csv_1_path <- paste(dir_path, "afc_tache_menageres.csv", sep="")
 
-dataP <- read.csv(csv_1_path, sep=";")
-dataP
+df <- read.csv(csv_1_path, sep=";")
 
-barplot(t(dataP[, -1]), beside =T, names = dataP$Task, col = c("lightblue", "mistyrose","lightcyan", "lavender"), legend = colnames(dataP [,2:5]))
+row.names(dataP) <- dataP$Task
+
+
+barplot(t(dataP[, -1]),
+        beside =T,
+        names = dataP$Task,
+        col = c("lightblue", "mistyrose","lightcyan", "lavender"),
+        legend = colnames(dataP[, 2:5]))
+
+library(FactoMineR)
+
+res = CA(dataP[, 2:5])
+
+plot.CA(res,
+        xax = 1,
+        yax = 2,
+        clabel=1.5)
+
+plot.CA(res,
+        axes = c(2,3))
+
+res$col
+sum(res$col$inertia) # 1.11494
